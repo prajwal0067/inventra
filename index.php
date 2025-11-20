32f9e4e2594fc3a6c2fc19495fa5d30108ce4fd7
prajwal0067 <prajwaldanappanavar@gmail.com>
2025-11-20 22:31:57 +0530

    Add files via upload
    signup

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+        <link rel="stylesheet" href="styleee.css">
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Aclonica&family=Poiret+One&display=swap');
+</style>
+</head>
+<body>
+    <div class="bod">
+    <div class="container">
+        <div id="logo" class="box"></div>
+        <div id="log" class="box">
+            
+            <h2>Login</h2>
+            <form action="process.php" method="post">
+                <div class="form-group">
+                    <label for="name">NAME</label>
+                    <input type="text" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="email">ENTER EMAIL</label>
+                    <input type="email" id="email" class="active">
+                </div>
+                
+               
+                <button class="sub" type="button" onclick="window.location.href='dashboard.php'">Submit</button>
+
+            </form>
+        
+        </div>
+    </div></div>
+    <div id="bg1"></div>
+    <div id="bg2"></div>
+    <div id="bg3"></div>
+    <div id="bg4"></div>
+     <div id="bg5"></div>
+
+    <div id="tag">The One-Stop Solution: Excellent suggestions, Manage sales, profits, and stock </div>
+
+
+    
+</body>
+</html>
